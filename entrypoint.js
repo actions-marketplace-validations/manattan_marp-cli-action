6a0d75c597dccb25a9785ca80147de0285e7d947
manattan <irjp.takanori777@gmail.com>
2023-06-19 20:54:30 +0900

    add

--- entrypoint.js
+++ entrypoint.js
@@ -133,6 +133,7 @@ async function marpCli(userConfigFilePath, config) {
     "--config-file",
     configPath,
     "-I",
+    "--html",
     path.join(path.dirname(userConfigFilePath), config.inputDir),
   ]);
 
